273c65fc5752eec11627c69b923171fda5525acb
Joshua Seitz <joshuabseitz@gmail.com>
2022-06-03 15:01:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,85 +1,150 @@
+# IMPORTS
+from datetime import datetime
 import csv
 import tweepy
+import time
 
-# Read inputs.json file
+# PROPERTIES
+data = []
+apiSwitchCount = 0
+
+# INPUTs
+usernames = []
+number_of_tweets = 30
+
+# JSON INIT
 import json
 file = open("input.json","r")
 inputs = json.loads(file.read())
 
-# Pulling search criteria from input.json
-number_of_tweets = 0
-
-# Pulling Twitter API information from input.json
+# API CREDS 1
 consumer_key=inputs["api_key"]
 consumer_secret=inputs["api_key_secret"]
 access_key=inputs["access_token"]
 access_secret=inputs["access_secret"]
 bearer_token=inputs["bearer_token"]
-username = ""
 
-# API Construction
+# API INIT
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_key,access_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True)
-client = tweepy.Client(bearer_token,wait_on_rate_limit=True)
-
-usernames = ["cassiuswinston","Cademac_12","Hesifambryson","_Greatest_Ever1","evinawestbrook"]
-number_of_tweets = 30
+api = tweepy.API(auth)
+client = tweepy.Client(bearer_token)
 
 def main():
 
+  global auth
+  global api
+  global client
+
+  readCSV()
+  
   for username in usernames:
+    
+    print("🔍 Evaluating ", username)
     getData(username)
+    # Convert the "Data" list to a spreadsheet
+    toCsv(data, username)
+
+def readCSV():
+
+  global usernames
+    
+  # Using readlines()
+  file1 = open('players.txt', 'r')
+  Lines = file1.readlines()
+
+  # Strips the newline character
+  for line in Lines:
+    line = line.replace("@","")
+    line = line.replace('\n', '')
+    usernames.append(line)
+
+def init(consumer_key, consumer_secret, access_key,access_secret,bearer_token):
+
+  global auth
+  global api
+  global client
+  
+  # API INIT
+  auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+  auth.set_access_token(access_key,access_secret)
+  api = tweepy.API(auth)
+  client = tweepy.Client(bearer_token)
+
+  print("        - Api + Client initialized with new credentials")
+  
+
+def switchAPI():
+
+  time.sleep(15)
+
+  global apiSwitchCount
+  global file
+  global inputs
+
+  if apiSwitchCount==0:
+    
+    # API CREDS 2
+    consumer_key=inputs["api_key"]
+    consumer_secret=inputs["api_key_secret"]
+    access_key=inputs["access_token"]
+    access_secret=inputs["access_secret"]
+    bearer_token=inputs["bearer_token"]
+    
+    init(consumer_key, consumer_secret, access_key,access_secret,bearer_token)
+
+    print("                - API Creds 1")
+    apiSwitchCount +=1
+    
+  elif apiSwitchCount == 1:
+    
+    # API CREDS 2
+    consumer_key=inputs["api_key_2"]
+    consumer_secret=inputs["api_key_secret_2"]
+    access_key=inputs["access_token_2"]
+    access_secret=inputs["access_secret_2"]
+    bearer_token=inputs["bearer_token_2"]
+    
+    init(consumer_key, consumer_secret, access_key,access_secret,bearer_token)
+    
+    print("                - API Creds 2")
+    apiSwitchCount += 1
+    
+  elif apiSwitchCount == 2:
+
+    # API CREDS 2
+    consumer_key=inputs["api_key_3"]
+    consumer_secret=inputs["api_key_secret_3"]
+    access_key=inputs["access_token_3"]
+    access_secret=inputs["access_secret_3"]
+    bearer_token=inputs["bearer_token_3"]
+    
+    init(consumer_key, consumer_secret, access_key,access_secret,bearer_token)
+    
+    print("                - API Creds 3")
+    apiSwitchCount = 0
 
 def getData(username):
+
+  # START Time
+  start = datetime.now().time()
     
   # Get a X amount of Tweets from the specified user
   tweet_IDs = getPaginatedTweets(username, number_of_tweets)
 
-  # get last Tweet for reference in replies request (i.e. get repliese since this last Tweet)
-  lastTweetId = tweet_IDs[len(tweet_IDs)-1]
-  print("Last Tweet ID: ", lastTweetId)
-
-  # get replies
-  repliers = getPaginatedReplies(lastTweetId)
-
-  replierCount = 0
-  
-  for fan in repliers:
-    replierCount += 1
-
-  print("Reply Count ", replierCount)
-
-  # Get all retweeters of each Tweet
-  retweeters = getPaginatedRts(tweet_IDs)
-
-  retweetersCount = 0
-  
-  for fan in retweeters:
-    retweetersCount += 1
-
-  print("Retweet Count ", retweetersCount)
-
-  # Get all likers of each Tweet
-  likers = getPaginatedLikes(tweet_IDs)
-
-  likersCount = 0
-  
-  for fan in likers:
-    likersCount += 1
-
-  print("Liker Count ", likersCount)
-
-  print("----- STORING FAN DATA -----")
-  data = []
+  userMetrics = [username,
+                 getFollowerCount(username),
+                 getLikes(tweet_IDs),
+                 getRetweets(tweet_IDs),
+                 getReplies(tweet_IDs),
+                 start,
+                 datetime.now().time(),
+                 number_of_tweets]
 
   # Add this fan and their data to the "Data" list
-  data.append([username, likersCount, retweetersCount, replierCount])
-      
-  # Convert the "Data" list to a spreadsheet
-  toCsv(data)
+  data.append(userMetrics)
   
-def toCsv(data):
+def toCsv(data, username):
 
   # Open or create a CSV file, with "w" or write permission
   f = open("data.csv", "w")
@@ -88,100 +153,164 @@ def toCsv(data):
   writer = csv.writer(f)
 
   # write the header
-  writer.writerow(["User","Total Likes", "Total Retweets", "Total Replies"])
+  writer.writerow(["User",
+                   "Follower Count",
+                   "Total Likes",
+                   "Total Retweets",
+                   "Total Replies",
+                   "Start Time",
+                   "End Time",
+                   "Number of Tweets Evaluated"])
 
   # write a new row for each fan and their data
   for fan in data:
     writer.writerow(fan)
   f.close()
 
-  print("----- Data written to 'data.csv' file -----")
-
-def getPaginatedReplies(lastTweetID):
-
-  repliers = []
-
-  print("----- GET REPLIES -----")
-
-  # Get replies
-  responsePages = tweepy.Cursor(api.search_tweets, q='to:{}'.format(username),
-                          since_id=lastTweetID,count=100).pages(number_of_tweets*10)
-
-  # for every reply, append their username
-  for response in responsePages:
-
-    print("fetching new response...")
-    
-    for reply in response:
-      
-      # print("Reply from:  ", reply.user.screen_name, " appended.")
-      repliers.append(reply.user.screen_name)
-  
-  return repliers
+  print("    - ✅ Data written to csv file ")
   
 def getPaginatedTweets(username, number_of_tweets):
-  
+
   tweetIdList = []
+  
+  print("    - Gathering Tweet IDs...")
 
-  print("----- RETRIEVING TWEET IDs -----")
-
-  # Captures all specified Tweets, paginates through multiple pages as needed
-  for tweet in tweepy.Cursor(api.user_timeline,screen_name=username,include_rts='false',count=200).items(number_of_tweets):
-
-    # appends the ID of each Tweet
-    tweetIdList.append(tweet.id)
-    
-    print(tweet.id)
+  while True:
+    try:
+      
+      for tweet in tweepy.Cursor(api.user_timeline,
+                                 screen_name=username,
+                                 include_rts='false',
+                                 count=200).items(number_of_tweets):
+        tweetIdList.append(tweet.id)
+      break
+    except tweepy.errors.Unauthorized:
+      print("        - Unauthorized")
+      break
+    except tweepy.errors.TooManyRequests:
+      print(("        - Too many requests... switching creds"))
+      switchAPI()
+      continue
+    except tweepy.errors.NotFound:
+      break
+    except:
+      time.sleep(900)
+      switchAPI()
 
   # return the entire list of Tweet IDs
   return(tweetIdList)
 
-def getPaginatedRts(tweetIdList):
+def getRetweets(tweetIdList):
   
-  print("----- RETRIEVING PAGINATED RETWEETERS -----")
+  retweets = 0
 
-  retweetersList = []
-
-  # for every Tweet
+  print("    - Counting retweets...")
   for tweet in tweetIdList:
+    while True:
+      try:
+        tweet = api.get_status(tweet)
+        retweets += tweet.retweet_count
+        break
+      except tweepy.errors.Unauthorized:
+        print("        - Unauthorized")
+        break
+      except tweepy.errors.TooManyRequests:
+        print(("        - Too many requests... switching creds"))
+        switchAPI()
+        continue
+      except tweepy.errors.NotFound:
+        break
+      except:
+        time.sleep(900)
+        switchAPI()
 
-    print("Evaluating: ", tweet, " for retweets.")
+  print("        - ", retweets, " Retweets")
+  return(retweets)
 
-    # get the retweeters and paginate as needed
-    for response in tweepy.Paginator(client.get_retweeters,
-                                     tweet):
-      # if the response from Twitter's API is not empty
-      if(response.meta['result_count']!=0):
+def getLikes(tweetIdList):
 
-        # append each retweeter to an array
-        for retweeter in response.data:
-          retweetersList.append(retweeter.username)
-
-  # reutrn the list of retweeters
-  return(retweetersList)
-
-def getPaginatedLikes(tweetIdList):
-
-  print("----- RETRIEVING PAGINATED LIKERS -----")
+  likes = 0
   
-  userLikeList = []
-
-  # for every Tweet captured
+  print("    - Counting favorites...")
   for tweet in tweetIdList:
     
-    print("Evaluating: ", tweet, " for favorites.")
+    while True:
+      try:
+        tweet = api.get_status(tweet)
+        likes += tweet.favorite_count
+        break
+      except tweepy.errors.Unauthorized:
+        print("        - Unauthorized")
+        break
+      except tweepy.errors.TooManyRequests:
+        print(("        - Too many requests... switching creds"))
+        switchAPI()
+        continue
+      except tweepy.errors.NotFound:
+        break
+      except:
+        time.sleep(900)
+        switchAPI()
+  
+  print("        - ", likes, " Likes")
+  return(likes)
 
-    # get the likers of the Tweet, paginate as needed
-    for response in tweepy.Paginator(client.get_liking_users,tweet):
+def getReplies(tweetIDList):
 
-      # if there are likers
-      if(response.meta['result_count']!=0):
+  replies = 0
 
-        # append them
-        for like in response.data:
-          userLikeList.append(like.username)
+  print("    - Counting replies...")
+  for tweet in tweetIDList:
 
-  # return the list of likers
-  return(userLikeList)
+    while True:
+      try:
+        client_result = client.get_tweet(tweet, \
+            tweet_fields=["public_metrics"])
+        tweet = client_result.data
+        replies += tweet.public_metrics["reply_count"]
+        break
+      except tweepy.errors.Unauthorized:
+        print("        - Unauthorized")
+        break
+      except tweepy.errors.TooManyRequests:
+        print(("        - Too many requests... switching creds"))
+        switchAPI()
+        continue
+      except tweepy.errors.NotFound:
+        break
+      except:
+        time.sleep(900)
+        switchAPI()
+
+  print("        - ", replies, " Replies")
+  return(replies)
+
+def getFollowerCount(fan):
+
+  followers = 0
+
+  print("    - Counting followers...")
+
+  while True:
+    try:
+      user = api.get_user(screen_name=fan)
+      followers = user.followers_count
+      break
+    except tweepy.errors.Unauthorized:
+      print("        - Unauthorized")
+      break
+    except tweepy.errors.TooManyRequests:
+      print(("        - Too many requests... switching creds"))
+      switchAPI()
+      continue
+    except tweepy.errors.NotFound:
+      break
+    except:
+      time.sleep(900)
+      switchAPI()
+
+  print("        - ", followers, " Followers")
+
+  return(followers)
 
 main()
